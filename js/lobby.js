ee2aa0c4fc59375444b9cc9c875ef5419eba776e
Marco Joaquin Barcela <mac7barcela@gmail.com>
2024-03-25 16:40:42 +0800

    set localStorage to sessionStorage instead

--- js/lobby.js
+++ js/lobby.js
@@ -1,7 +1,7 @@
 let form = document.getElementById("lobby__form");
 
 //get the name of the user via localStorage
-let displayName = localStorage.getItem("display_name");
+let displayName = sessionStorage.getItem("display_name");
 if (displayName) {
   form.name.value = displayName;
 }
@@ -10,7 +10,7 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   //set the name value after submit it
-  localStorage.setItem("display_name", e.target.name.value);
+  sessionStorage.setItem("display_name", e.target.name.value);
 
   //need to get the roomName
   let inviteCode = e.target.room.value;
